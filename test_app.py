3291d958faeee93bec07418318b892b3bfd000d2
Assaf Stone <1774556+devopsjester@users.noreply.github.com>
2024-04-19 16:46:24 +0000

    add tests and CI

---
+++ test_app.py
@@ -0,0 +1,21 @@
+import unittest
+from python_webapp_flask import app
+
+
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home_status_code(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page(self):
+        response = self.app.get("/about")
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
